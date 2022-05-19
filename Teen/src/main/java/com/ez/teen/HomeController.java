7f95387539119b6bf6f2f75c81c8cf0419a1f1a3
Chori <cjfcksgml@gmail.com>
2022-05-19 23:37:48 +0900

    Merge branch 'develop' of https://github.com/give57ao/teencoder.git into develop

--- Teen/src/main/java/com/ez/teen/HomeController.java
+++ Teen/src/main/java/com/ez/teen/HomeController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 =======
 //hihihi
 //HIHIHIHI
-//hihihihihihihi
+//hihihihihihihihi
 >>>>>>> main
 @Controller
 public class HomeController {
