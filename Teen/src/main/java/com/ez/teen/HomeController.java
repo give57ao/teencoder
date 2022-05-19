b9bc63c83da4e841b988b7dfc4ccc8a66b36d33c
ho_ong8 <honghj8959@gmail.com>
2022-05-19 21:31:01 +0900

    asdasd

--- Teen/src/main/java/com/ez/teen/HomeController.java
+++ Teen/src/main/java/com/ez/teen/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page. test T E S T aaaaa aaaa aaa
  */
+//hi
 @Controller
 public class HomeController {
 	
